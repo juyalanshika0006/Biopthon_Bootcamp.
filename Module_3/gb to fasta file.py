a77d6597537cd81d7a34f0d4d0109313ed5410bf
A.J <juyalanshika6@gmail.com>
2026-07-29 18:16:03 +0530

    Refactor script to convert GenBank to protein FASTA
    Updated the script to read a GenBank file, extract the CDS feature, translate it to a protein sequence, and save it in FASTA format.

--- Module_3/gb to fasta file.py
+++ Module_3/gb to fasta file.py
@@ -1,10 +1,36 @@
 from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
 
-SeqIO.convert(
+
+record = SeqIO.read(
     "Module_3/BRCA1_record.gb",
-    "genbank",
-    "Module_3/BRCA1_record.fasta",
-    "fasta"
+    "genbank"
 )
 
-print("Conversion completed!")
+for feature in record.features:
+
+    if feature.type == "CDS":
+
+        
+        cds_sequence = feature.extract(record.seq)
+
+        
+        protein_sequence = cds_sequence.translate(
+            to_stop=True
+        )
+
+        
+        protein_record = SeqRecord(
+            protein_sequence,
+            id="BRCA1_protein",
+            description="BRCA1 protein sequence"
+        )
+
+        
+        SeqIO.write(
+            protein_record,
+            "Module_3/BRCA1_protein.fasta",
+            "fasta"
+        )
+
+        print("Protein FASTA created successfully!")

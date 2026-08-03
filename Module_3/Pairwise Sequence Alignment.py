8b114057a4de8c41f5bab5f56631dfc21b281143
A.J <juyalanshika6@gmail.com>
2026-08-03 14:31:40 +0530

    Implement global and local alignment in script
    Added global and local alignment modes with scoring.

--- Module_3/Pairwise Sequence Alignment.py
+++ Module_3/Pairwise Sequence Alignment.py
@@ -20,3 +20,37 @@ print(
     alignments[0]
 )
 print(alignments.score)
+
+print( "-" * 50)
+
+
+#________________________________________________________________________________________________________
+
+aligner=PairwiseAligner()
+aligner.mode = "global"
+aligner.match_score=1
+aligner.mismatch_score=-1
+alignment=aligner.align(
+    sequence1,
+    sequence2
+)[0]
+print(alignment)
+print("score:", alignment.score)
+
+print( "-" * 50)
+
+#________________________________________________________________________________________________________
+aligner=PairwiseAligner()
+aligner.mode = "local"
+aligner.match_score=1
+aligner.mismatch_score=-1
+alignment=aligner.align(
+    sequence1,
+    sequence2
+)[0]
+print(alignment)
+print("score:", alignment.score)
+
+print( "-" * 50)
+
+
